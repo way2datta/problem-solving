1a7c92b6e7b8ea724711dcd2eb3229e17ebd6d95
Dattatray Kale <way2datta@gmail.com>
2019-08-05 21:43:44 +0530

    Setup for test

---
+++ src/String.test.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+describe("String", () => {
+    describe("#contains()", () => {
+        it("should return true when search string is present in given string", () => {
+            expect(true).to.be.equal(false);
+        })
+    })
+})
