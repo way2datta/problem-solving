02894f87ff0d11ae2715241d2541296c2422bed6
Dattatray Kale <way2datta@gmail.com>
2019-08-05 21:52:00 +0530

    Added failing test for _String.contains

--- src/String.test.js
+++ src/String.test.js
@@ -1,8 +1,17 @@
 import { expect } from "chai";
+import String from "./String";
+
 describe("String", () => {
-    describe("#contains()", () => {
-        it("should return true when search string is present in given string", () => {
-            expect(true).to.be.equal(false);
-        })
-    })
-})
+  describe("#contains()", () => {
+    it("should return true when search string is present in given string", () => {
+      // Arrange
+      var value ="In the future, Earth is slowly becoming uninhabitable. Ex-NASA pilot Cooper, along with a team of researchers, is sent on a planet exploration mission to report which planet can sustain life."
+
+      // Act
+     var containsSearchString = String.contains(value, "become");
+
+     // Assert
+     expect(containsSearchString).to.be.true;
+    });
+  });
+});
