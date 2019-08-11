38ee8dcccb52b7c4bce8d6577a6c340701cdc9df
Dagttatray Kale <way2datta@gmail.com>
2019-08-11 14:45:48 +0530

    Available symbols

--- javascript/src/GuidGenerator.js
+++ javascript/src/GuidGenerator.js
@@ -1,8 +1,9 @@
 export default {
   generate: (length = 32) => {
-    const availableSymbols = "0ab2cOefgBhijkSln1oYqJstuvwx9yzACDpEFr8GH5ImKLMNdPQRTUVWXZ3467";
+    const availableSymbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const charactersLength = availableSymbols.length;
     const newSymbols = [];
+
     for (let index = 0; index < length; index++) {
       const randomNumber = Math.floor(Math.random() * charactersLength);
       const randomChar = availableSymbols.charAt(randomNumber);
