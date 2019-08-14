cdf3e7682a3c06b2b1672980a0536bffe099a73f
Dagttatray Kale <way2datta@gmail.com>
2019-08-14 18:43:10 +0530

    Refactor

--- javascript/src/NumberStringManipulation.js
+++ javascript/src/NumberStringManipulation.js
@@ -1,24 +1,19 @@
 // Given a string str containing alphanumeric characters, calculate sum of all numbers present in the string.
 export function sumOfAllNumbersInString(text) {
-
-  let sum = 0;
-  let currentNumber = 0;
-  let digitCount = 0;
+  var sum = 0;
+  var lastNumber = 0;
 
   for (let index = 0; index < text.length; index++) {
-    const char = text.charAt(index);
+    const char = text[index];
+
     if (!isNaN(char)) {
-      let factor = digitCount == 0 ? 0 : 10;
-      currentNumber = currentNumber * factor + +char;
-      digitCount++;
-      continue;
+      var factor = lastNumber ? 10  : 0;
+      lastNumber = lastNumber * factor + +char;
     }
-    if (currentNumber > 0) {
-      sum += currentNumber;
-      currentNumber = 0;
-      digitCount = 0;
+    else {
+      sum = sum + lastNumber;
+      lastNumber = 0;
     }
   }
-  sum += currentNumber;
-  return sum;
+  return sum + lastNumber;
 }
