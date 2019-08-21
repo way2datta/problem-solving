fb2c6bfdb1c6a814e4e0b1fd8ef266bc354f6fc3
Dattatray Kale <way2datta@gmail.com>
2019-08-21 20:19:20 +0530

    Given expression contains balanced paranthesis

---
+++ csharp/problem-solving/problem-solving/StringManipulations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    public class StringManipulations
+    {
+        public static bool ContainsBalancedParanthesis(string expression)
+        {
+            Stack<char> braces = new Stack<char>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                var letter = expression[i];
+                if (letter == '{' || letter == '(' || letter == '[')
+                {
+                    braces.Push(letter);
+                    continue;
+                }
+
+                char topBrace = braces.Peek();
+
+                if ((letter == '}' && topBrace == '{') ||
+                    (letter == ')' && topBrace == '(') ||
+                    (letter == ']' && topBrace == '['))
+                {
+                    braces.Pop();
+                }
+            }
+
+            return braces.Count == 0;
+        }
+    }
+}
